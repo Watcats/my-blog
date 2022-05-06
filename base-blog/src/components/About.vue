bc93b8d9052df68aa147adc24b64532696ced349
Watcats <1394297687@qq.com>
2022-05-06 17:30:57 +0800

    先弄个计时器试试

---
+++ base-blog/src/components/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <img alt="Vue logo" src="../assets/logo.png" />
+  <h1>{{ msg }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'About',
+  data() {
+    return {
+      msg: 'Hello Vue 3.0 + Vite!'
+    }
+  },
+  // setup() { }
+})
+</script>
